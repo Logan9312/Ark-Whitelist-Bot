2fad4ff88e860f4de4f47ca7ec37c0660ea07de2
Logan <lgntravis@gmail.com>
2024-01-23 21:31:44 -0700

    Changing to Go

---
+++ bot/base.go
@@ -0,0 +1,34 @@
+package bot
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var commands = []*discordgo.ApplicationCommand{}
+
+func BotConnect(token string) (*discordgo.Session, error) {
+
+	s, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return s, fmt.Errorf("Discordgo.New Error: %w", err)
+	}
+
+	s.Identify.Intents = discordgo.IntentsAllWithoutPrivileged | discordgo.IntentsGuildMembers
+
+	err = s.Open()
+	if err != nil {
+		return s, fmt.Errorf("failed to open a websocket connection with discord. Likely due to an invalid token. %w", err)
+	}
+
+	s.ApplicationCommandBulkOverwrite(s.State.User.ID, "", commands)
+
+	fmt.Println(s.State.User.Username + " bot startup complete!")
+
+	return s, nil
+}
+
+func Ptr[T any](v T) *T {
+	return &v
+}
